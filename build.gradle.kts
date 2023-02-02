e816a15cc13586de5d041e26ca47d39a3fb815e0
Roman Elizarov <elizarov@gmail.com>
2023-02-02 21:55:55 +0300

    Continuous resolve

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.7.21"
+    kotlin("multiplatform") version "1.8.0"
 }
 
 group = "org.example"
@@ -10,20 +10,9 @@ repositories {
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    nativeTarget.apply {
+    linuxX64("native") {
         binaries {
-            executable {
-                entryPoint = "main"
-            }
+            executable()
         }
     }
     sourceSets {
--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,34 +1,60 @@
 import kotlinx.cinterop.*
 import platform.posix.*
+import kotlin.time.*
+import kotlin.time.TimeSource.*
 
-fun main(args: Array<String>) {
-    if (args.size != 1) {
-        println("Usage: addrinfo <node>")
-        return
+value class Addr(private val addr: UInt) {
+    override fun toString() = buildString {
+        for (i in 0 until 4) {
+            if (i != 0) append('.')
+            append((addr shr (i * 8)).toUByte())
+        }
     }
-    val node = args[0]
+}
+
+fun resolveHostAddr(host: String): List<Addr> = buildList {
     memScoped {
         val hints: addrinfo = alloc()
         hints.ai_family = AF_INET
         hints.ai_socktype = SOCK_DGRAM
         val res: CPointerVar<addrinfo> = alloc()
-        val err = getaddrinfo(node, null, hints.ptr, res.ptr)
+        val err = getaddrinfo(host, null, hints.ptr, res.ptr)
         if (err != 0) {
             println("addrinfo error: ${gai_strerror(err)?.toKString()}")
-            return
+            return@buildList
         }
         var cur = res.value
         while (cur != null) with (cur.pointed) {
-            val addr = ai_addr!!.reinterpret<sockaddr_in>().pointed.sin_addr.s_addr
-            val str = buildString {
-                for (i in 0 until 4) {
-                    if (i != 0) append('.')
-                    append((addr shr (i * 8)).toUByte())
-                }
-            }
-            println(str)
+            add(Addr(ai_addr!!.reinterpret<sockaddr_in>().pointed.sin_addr.s_addr))
             cur = ai_next
         }
         freeaddrinfo(res.value)
+    }
+}
+
+private const val ESC = 27.toChar()
+
+@OptIn(ExperimentalTime::class)
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("Usage: addrinfo <host>")
+        return
+    }
+    val host = args[0]
+    var lastLines = 0
+    val known = LinkedHashMap<Addr, Monotonic.ValueTimeMark>()
+    var count = 0
+    while (true) {
+        repeat(lastLines) { print("$ESC[A") }
+        println("Resolving $host #${++count}")
+        val list = resolveHostAddr(host)
+        val now = Monotonic.markNow()
+        for (addr in list) known[addr] = now
+        for ((addr, mark) in known) {
+            val seen = if (mark == now) "now" else "${mark.elapsedNow().toString(DurationUnit.SECONDS)} sec ago"
+            println("$ESC[2K$addr - $seen")
+        }
+        lastLines = known.size + 1
+        sleep(1)
     }
 }
